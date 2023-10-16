1dffe2416d469869d3343f4690083fe5a7f080a0
ju2ta2 <alexnn5@gmail.com>
2023-10-17 01:25:46 +0300

    java-example: Update GeoZonesPageTest.java

--- java-example/src/main/java/com/nostromo/GeoZonesPageTest.java
+++ java-example/src/main/java/com/nostromo/GeoZonesPageTest.java
@@ -22,15 +22,18 @@ public class GeoZonesPageTest extends BaseTest{
     @Test
     public void test02GeoZonesCount() {
         driver.findElement(By.cssSelector("[href*='litecart/admin/?app=geo_zones&doc=geo_zones']")).click();
-        String countriesListSize = "Geo Zones: " + driver.findElements(By.cssSelector("[class=link]")).size();
-        String countriesCount = driver.findElement(By.cssSelector("tfoot")).getAttribute("innerText");
-        assertEquals(countriesListSize, countriesCount);
+        String zonesListSize = "Geo Zones: " + driver.findElements(By.cssSelector("[class=link]")).size();
+        String zonesCount = driver.findElement(By.cssSelector("tfoot")).getAttribute("innerText");
+        assertEquals(zonesListSize, zonesCount);
     }
 
     @Test
     public void test03ListAlphabeticallyOrder() {
+        driver.findElement(By.cssSelector("[href*='litecart/admin/?app=geo_zones&doc=geo_zones']")).click();
         List allGeoZonesList = new ArrayList<>();
-        for (int i = 0; i < driver.findElements(By.cssSelector("[class=link]")).size(); i++) {
+        WebElement zonesTable = driver.findElement(By.cssSelector("table[class='table table-striped table-hover data-table']"));
+        int zonesSize =  zonesTable.findElements(By.cssSelector("a[class='link']")).size();
+        for (int i = 0; i < zonesSize; i++) {
             allGeoZonesList.add(driver.findElements(By.cssSelector("[class=link]")).get(i).getAttribute("textContent"));
         }
         isListOrdered(allGeoZonesList);
@@ -38,20 +41,19 @@ public class GeoZonesPageTest extends BaseTest{
 
     @Test
     public void test04CountryListAlphabeticallyOrder() {
-        List countriesList = new ArrayList<String>();
-        for (int i = 0; i < driver.findElements(By.xpath("//a[@class='link']")).size(); i++) {
-            WebElement listElement = driver.findElements(By.xpath("//a[@class='link']")).get(i);
-            WebElement zoneCountElement = listElement.findElement(By.xpath("//td[@class='text-center']"));
-            String zoneValue = zoneCountElement.getAttribute("textContent");
-            String countryName = listElement.getAttribute("textContent");
-//            if(Integer.parseInt(zoneValue) != 0) {
-//                listElement.click();
-//                int zonesCount = driver.findElements(By.cssSelector("[class='table table-striped table-hover data-table'] [name$='[name]']")).size();
-//                for (int j = 0; j < zonesCount; j++) {
-//                    zoneCountriesList.add(driver.findElement(By.cssSelector("[class='table table-striped table-hover data-table'] [name$='[name]']")).getAttribute("value"));
-//                }
-//                isListOrdered(zoneCountriesList);
-//            }
+        driver.findElement(By.cssSelector("[href*='litecart/admin/?app=geo_zones&doc=geo_zones']")).click();
+        WebElement zonesTable = driver.findElement(By.cssSelector("table[class='table table-striped table-hover data-table']"));
+        int zonesSize =  zonesTable.findElements(By.cssSelector("a[class='link']")).size();
+        for (int i = 0; i < zonesSize; i++) {
+            zonesTable.findElements(By.xpath("//a[@class='link']")).get(i).click();
+            WebElement countryTable = driver.findElement(By.cssSelector("table[class='table table-striped table-hover data-table']"));
+            int countrySize =countryTable.findElements(By.cssSelector("input[name$='[country_code]']")).size();
+            List countriesList = new ArrayList<String>();
+            for (int j = 0; j < countrySize; j++) {
+                //TODO get country name
+                countriesList.add(countryTable.findElements(By.cssSelector("input[name$='[country_code]']")).get(j).getAccessibleName());
+            }
+            isListOrdered(countriesList);
         }
     }
 }
